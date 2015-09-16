429e66a17fbd4cb8c6ce7eed73c3babd75b98afd
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2015-09-16 00:51:02 -0500

    added bootstrap form to user#edit; bug in _wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,5 +1,36 @@
 class WikisController < ApplicationController
+   def index
+    @wikis = Wiki.all
+   end
+
    def new
     @wiki = Wiki.new
    end
- end
+
+  #  def show
+  #   @wiki = Wiki.find(params[:id])
+  #  end
+
+  #  def edit
+  #   @wiki = Wiki.find(params[:id])
+  #  end
+
+  #  # def create
+  #  #  @wiki = Wiki.new(params.require(:wiki).permit(:title, :body, :public))
+  #  #  if @wiki.save
+  #  #    redirect_to @wiki, notice: "New Wiki was created successfully."
+  #  #  else
+  #  #    flash [:error] = "There was an error creating the new Wiki. Please try again."
+  #  #  end
+  #  # end
+
+  #  def update
+  #   @wiki = Wiki.find(params[:id])
+  #   if @wiki.update_attributes(params.require(:topic).permit(:title, :body, :public))
+  #     redirect_to @wiki
+  #   else
+  #     flash[:error] = "Error saving topic. Please try again."
+  #     render :edit
+  #   end
+  # end
+end
